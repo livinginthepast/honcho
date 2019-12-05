1e82af6fc1111a922a3cb9486671043bea33996c
Eric Saxby <sax@livinginthepast.org>
2019-12-05 13:41:00 -0800

    Fix warning

--- lib/honcho/application.ex
+++ lib/honcho/application.ex
@@ -1,7 +1,7 @@
 defmodule Honcho.Application do
   use Application
 
-  def start(_type, args) do
+  def start(_type, _args) do
     Honcho.Output.warn("starting…")
     Honcho.CommandSupervisor.start_link()
   end
